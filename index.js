79b9ae840ce17b167d00b5c50f34e18803e0c3c2
Daniel Russ <druss@mail.nih.gov>
2023-04-14 19:34:05 +0000

    reading from bucket

--- index.js
+++ index.js
@@ -24,28 +24,48 @@ functions.http('myHttpFunction', async (req, res) => {
     // note: this does not actually create a file...
     const newfile = bucket.file('This-Is-A-Test-File.json');
     resp += `<br>${newfile.name} ${newfile.cloudStorageURI}`
-    let json={
-        'obj1':{
-            name:"Daniel",
+    let json = {
+        'obj1': {
+            name: "Daniel",
             action: "was here"
         }
     }
-    await newfile.save(JSON.stringify(json) )
+    await newfile.save(JSON.stringify(json))
 
     // Send an HTTP response
     res.send(resp);
 });
 
-functions.http('s2',async (req,res)=>{
-    const storage = new Storage({ keyFilename: "/Users/druss/.gcp/nih-nci-dceg-druss-91b670657c7a.json" });
+async function read_file(file) {
+    // https://stackoverflow.com/questions/51970205/how-to-read-content-of-json-file-uploaded-to-google-cloud-storage-using-node-js
+    // https://stackoverflow.com/questions/58431076/how-to-use-async-await-with-fs-createreadstream-in-node-js
+    let buf = ""
+    let done = false
+    return new Promise((resolve, reject) => {
+        file.createReadStream()
+            .on('data', d => (buf += d))
+            .on('end', () => {
+                console.log(".... end ... \n", buf)
+                resolve(buf)
+            })
+            .on('error', e => reject(e))
+    })
+}
 
-    const bucketName = 'nih-nci-dceg-druss-bucket1'
-    const bucket = storage.bucket(bucketName)
-    console.log(bucket)
-    const [files] = await bucket.getFiles();
-    let resp = 'Ok<br>List of files in the bucket:<ul>'
-    files.forEach(file => resp = `${resp}<li>${file.name}</li>`)
-    resp += '</ul>'
+functions.http('s2', async (req, res) => {
+        const storage = new Storage({ keyFilename: "/home/druss/nih-nci-dceg-druss-91b670657c7a.json" });
 
-    res.send(resp);
-})
+        const bucketName = 'nih-nci-dceg-druss-bucket1'
+        const bucket = storage.bucket(bucketName)
+        console.log(bucket)
+        const [files] = await bucket.getFiles();
+        let resp = 'Ok<br>List of files in the bucket:<ul>'
+        files.forEach(file => resp = `${resp}<li>${file.name}</li>`)
+        resp += '</ul><hr> read the contents of data/This-Is-A-Test-File.json:'
+
+        let file = bucket.file('data/This-Is-A-Test-File.json')
+        let txt = await read_file(file)
+        resp += `<pre>${txt}</pre>`
+
+        res.send(resp);
+    })
